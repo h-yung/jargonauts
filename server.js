886344c3e34ffc931982e0df0ebd87ac94fb918a
h-yung <helen.yung.h@gmail.com>
2022-06-10 15:29:01 -0400

    first commit

--- server.js
+++ server.js
@@ -1,92 +1,19 @@
-if(process.env.NODE_ENV!=='production'){
-    const dotenv = require('dotenv')
-    dotenv.config({path: 'vars.env'})
-}
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-const cors = require('cors')
 
-MongoClient.connect(process.env.DATABASE_URL, { useUnifiedTopology: true})
-    .then(client => {
-        console.log('connected to database')
-        const db = client.db('jargonauts')
-        const jargonCollection = db.collection('jargonTerms')
-        app.use(cors())
-        app.use(express.urlencoded({ extended: true }))
-        app.use(express.json())
-        app.use(express.static('public'))
-        app.set('view engine','ejs')
-        
-        //tip of day on load
-        app.get('/jargon', (req,res) => { 
-            jargonCollection.find(
-                {
-                    "category": "tips"
-                }
-            )
-                .toArray()
-                .then(data => {
-                    res.render('index.ejs', { tips: data })
-                })
-                .catch(err => console.log(err))
-        })
-        // phrase recs
-        app.get('/tone/:tonalChoice', (req,res) => { 
-            const tonalChoice = req.params.tonalChoice.toLowerCase()
-            jargonCollection.find(
-                {
-                    "category": "tone",
-                    "toneOption": tonalChoice
-                }
-            )
-                .toArray()
-                .then(data => {
-                    res.json(data)
-                    // res.render('index.ejs', {}) for template eng
-                })
-                .catch(err => console.log(err))
-        })
-        // get dictionary
-        app.get('/dictionary', (req,res) => { 
-            jargonCollection.find(
-                {
-                    "category": "dictionary",
-                }
-            )
-                .toArray()
-                .then(data => {
-                    res.json(data)
-                    // res.render('index.ejs', {}) for template eng
-                })
-                .catch(err => console.log(err))
-        })
 
-        app.get('/api', (req,res)=> {
-            jargonCollection.find().toArray()
-                .then(data => {
-                    res.json(data)
-                })
-                .catch(err => console.log(err))
-        })
 
-        app.post('/jargon', (req,res)=> {
-            if (req.body.passcode != 112){
-                console.log(`Need passcode`) 
-                res.redirect('/')
-            }else {
-                jargonCollection.insertOne(req.body)
-                    .then(result => {
-                        console.log(result)
-                        res.redirect('/')
-                    })
-                    .catch(err => console.log(err))
-            }
-        })
 
-        app.listen(process.env.PORT || 8000, ()=> {
-            console.log(`Server now running`)
-        })
+app.set('view engine','ejs')
+app.use(express.static('public'))
 
-    })
-    .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+    // res.sendFile(__dirname + '/index.html')
+    // when you're using ejs - below -->
+    res.render('index.ejs', { info: data })
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Server running on port 3000 or other')
+})
